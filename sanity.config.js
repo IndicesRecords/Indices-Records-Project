0223c67acdb68780203551344faf0e1c11af14ca
IndicesRecords <indices.records@gmail.com>
2024-11-22 14:39:20 +0100

    Removed nested git repository (indices-records-project) from staging

---
+++ sanity.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'sanity';
+
+export default defineConfig({
+  projectId: 'nnxl5s4y', // Replace with your actual Project ID
+  dataset: 'production',       // Replace if using a different dataset
+  title: 'Indices Records',
+  plugins: [],
+});
